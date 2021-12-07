317a6c3bc3e476a3fe2e7a20a1aff3282c28c0fc
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-12-07 10:30:13 +0700

    read information in bootsector successfully

---
+++ BootSector.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+public class BootSector {
+	private String filePath;
+	private ByteBuffer bytes;
+	private HashMap<String, Object> fields = new HashMap<String, Object>();
+
+
+	public BootSector(String filePath) throws FileNotFoundException, IOException{
+		this.filePath = filePath;
+		var in = new FileInputStream(this.filePath);
+		bytes = ByteBuffer.wrap(in.readNBytes(512));
+		bytes.order(ByteOrder.LITTLE_ENDIAN);
+		fields.put("oemName", getAsciiString(3, 8));
+		fields.put("bytesPerSector", bytes.getShort(11));
+		fields.put("sectorsPerCluster", bytes.get(13));
+		fields.put("numReservedSectors", bytes.getShort(14));
+		fields.put("numFats", bytes.get(16));
+		fields.put("mediaType", bytes.get(21));
+		fields.put("sectorsPerTrack", bytes.getShort(24));
+		fields.put("numHeads", bytes.getShort(26));
+		fields.put("numTotalSectors", bytes.getInt(32));
+		fields.put("sectorsPerFat", bytes.getInt(36));
+		fields.put("rootDirStartCluster", bytes.getInt(44));
+		fields.put("volumeSerialNumber", bytes.getInt(67));
+		fields.put("volumeLabel", getAsciiString(71, 11));
+		fields.put("optionalFileSystemTypeLabel", getAsciiString(82, 8));
+
+	}
+
+	private String getAsciiString(int index, int size){
+		byte[] data = new byte[size];
+		bytes.get(index, data);
+		return new String(data, StandardCharsets.US_ASCII);
+	}
+
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		var bs = new BootSector(args[0]);
+		System.out.println(bs.fields);
+
+	}
+}
